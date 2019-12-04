a48e32671f64eb97b0de5df5cd588ca0dc758799
mathieumatos <mathieumatos1203@gmail.com>
2019-12-04 12:59:02 -0500

    finished recursive binaryTree generating function from newick string format

--- newick_binaryTree.py
+++ newick_binaryTree.py
@@ -1,36 +1,53 @@
 import struct
 
-str = "((PCDHA1_Humain, OR2J3_Humain), ((PCDHA1_Rat, PCDHA1_Souris), PCDHA1_Bonobo));"
-
-# root = struct.Node(1)
-
 def treeBuilder(nw):
+    if(nw[len(nw)-1]==";"):
+        nw = nw[:-1]
+
     if nw[0] == "(":
-        node = struct.Node(1)
+        node = struct.Node("1")
         nextLeft = ""
         nextRight = ""
-        parenthCounter = 0
+        nbParenthese = 0
         rightIndex = 0
         for s in range(1,len(nw)):
             if nw[s] == "(":
-                parenthCounter = parenthCounter + 1
+                nbParenthese = nbParenthese + 1
             elif nw[s] == ")":
-                parenthCounter = parenthCounter - 1
+                nbParenthese = nbParenthese - 1
             
-            if (nw[s] == ",") and (parenthCounter == 0):
-                node.left = treeBuilder(nextLeft)
+            if (nw[s] == ",") and (nbParenthese == 0):
+                if nextLeft[0] == "(":
+                    node.left = treeBuilder(nextLeft)
+                else:
+                    node.left = struct.Node(nextLeft)
                 rightIndex = s+2
                 break
-
+ 
             nextLeft = nextLeft + nw[s]
 
         nextRight = nw[rightIndex:-1]
-        print("NEXT LEFT = "+nextLeft)
-        print("NEXT RIGHT = "+nextRight)
-            
+        if nextRight[0] == "(":
+            node.right = treeBuilder(nextRight)
+        else:
+            node.right = struct.Node(nextRight)
+
+    return node
 
 
+# TESTS
+str = "((PCDHA1_Humain, OR2J3_Humain), ((PCDHA1_Rat, PCDHA1_Souris), PCDHA1_Bonobo));"
 
-treeBuilder(str)
-
-# root.printStruct()
+tree = treeBuilder(str)
+tree.printStruct()
+print(tree.postOrderTraversal(tree))
+print("TREE MANUAL SEARCH")
+print("tree.data = " +(tree.data))
+print("tree.left.data = " +(tree.left.data))
+print("tree.right.data = " +(tree.right.data))
+print("tree.left.left.data = " +(tree.left.left.data))
+print("tree.left.right.data = " +(tree.left.right.data))
+print("tree.right.left.data = " +(tree.right.left.data))
+print("tree.right.right.data = " +(tree.right.right.data))
+print("tree.right.left.left.data = "+(tree.right.left.left.data))
+print("tree.right.left.right.data = "+(tree.right.left.right.data))
--- struct.py
+++ struct.py
@@ -11,19 +11,19 @@ class Node:
         self.data = data
 
     def insert(self, data):
-        if self.data:
-            if data < self.data:
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.left.insert(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.right.insert(data)
-        else:
-            self.data = data
+        # if self.data:
+        #     if data < self.data:
+        #         if self.left is None:
+        #             self.left = Node(data)
+        #         else:
+        #             self.left.insert(data)
+        #     elif data > self.data:
+        #         if self.right is None:
+        #             self.right = Node(data)
+        #         else:
+        #             self.right.insert(data)
+        # else:
+        self.data = data
 
     # prints in ascending order
     def printStruct(self):
