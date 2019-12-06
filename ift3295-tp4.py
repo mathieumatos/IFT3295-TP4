7daca8e9cbcbb1c6d6226b4fd924579053a25b13
mathieumatos <mathieumatos1203@gmail.com>
2019-12-06 02:26:30 -0500

    added mapping of newick keys to values

--- ift3295-tp4.py
+++ ift3295-tp4.py
@@ -8,12 +8,14 @@ import sys
 import newick_binaryTree
 
 
+""" Functions """
+
 # Reads newick files and filters out newlines. Returns a list
 def readNewickFile(filename):
     return list(filter(None, open(filename).read().splitlines()))
 
 
-# Reads fasta files and returns a dictionnary
+# Reads fasta files and returns a dictionary
 def readFastaFile(filename):
     fastaDict = {}
     file = open(filename).read().splitlines()
@@ -37,6 +39,7 @@ def readFastaFile(filename):
     return fastaDict
 
 
+# Returns a list of the values of a dictionary
 def getValues(dict):
     values = []
     for i in dict:
@@ -44,7 +47,20 @@ def getValues(dict):
     return values
 
 
+# Returns a list of the names of a dictionary
+def getNames(dict):
+    return list(dict.keys())
 
+
+# Makes a dictionary from a list of keys and a list of respective values
+def makeDictionary(keys, values):
+    newDict = {}
+    for element in range(len(keys)):
+        newDict.update({keys[element]: values[element]})
+    return newDict
+
+
+# Removes gaps from a list of multiple sequences
 def removeGaps(seqs):
     newSeqs = seqs.copy()
     gapIndexTab = []
@@ -65,17 +81,44 @@ def removeGaps(seqs):
     return newSeqs
         
 
+# Converts a newick string (keys -> values) from dictionary
+def proteinKeyToValue_NewickString(nw, dictionary):
+    for i in range(len(dictionary)):
+        nw = nw.replace(list(dictionary.keys())[i],list(dictionary.values())[i])
+    return nw
 
-arbres = readNewickFile("arbres.nw")
+
+# Converts a list of newick strings (keys -> values) from dictionary
+def proteinKeyToValue_NewickList(nwList, dictionary):
+    for i in range(len(nwList)):
+        nwList[i] = proteinKeyToValue_NewickString(nwList[i], dictionary)
+    return nwList
+
+
+
+""" Gestion des proteines: Creation de dictionaire sans gaps """
 proteines = readFastaFile("proteines.fasta")
 # print(proteines)
 proteinesValues = getValues(proteines)
 # print(proteinesValues)
-noGapProteins = removeGaps(proteinesValues)
-print(noGapProteins)
+proteinesNames = getNames(proteines)
+# print(proteinesNames, proteinesValues)
+noGapProteinesValues = removeGaps(proteinesValues)
+# print(noGapProteinesValues)
+proteinesDictionary = makeDictionary(proteinesNames, noGapProteinesValues)
+# print(proteinesDictionary)
 
 
-# mini tests
+
+""" Gestion des arbres: Remplacement (key -> value) dans les arbres newick """
+arbres = readNewickFile("arbres.nw")
+# print(arbres)
+arbresProteines = proteinKeyToValue_NewickList(arbres,proteinesDictionary)
+# print(arbresProteines)
+
+
+
+""" Tests """
 # tab = ["ab-vd","d-uvg","5-555","-9999","00--0","11111"]
 # noGapsTab = removeGaps(tab)
 # print(noGapsTab)
