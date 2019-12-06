20c1bccaeaf946e804a4cdb0f502b07158cb8edf
nicolasrichard2 <nicolasmrichard@gmail.com>
2019-12-06 17:30:02 -0500

    premiere tentative de neighbor-joining

--- ift3295-tp4.py
+++ ift3295-tp4.py
@@ -95,6 +95,138 @@ def proteinKeyToValue_NewickList(nwList, dictionary):
     return nwList
 
 
+""" CODE QUE NICO A AJOUTÉ """
+
+seqs = ["INDNPPVFRGREQIIFIPESSRFPIEDADIGANALLTYTLELSKSLWLELRKYLDREETPELHLLLTATDGGKPELQGTVELLITVLDVNDNAPLFDQAVYRVHLL"
+        "ETLVTTLNASDADEGVNGEVVFSFDSGISRDFKVDSSSGEIRLIDKLKVLDVNDNAPELAVTSLYLPIREDAPLSTVIALITVSRDSGANGQVTCSLMPHVPFKLVS",
+        "MNDDGKVNASSEGYFILVGFSNWPHLEVVIFVVVLIFYLMTLIGNLFIIILSYLDSHLHTPMYFFLSYTTSSIPQYAGCMIVLLVVMSYDRYAAVCRPLHYTVLMH"
+        "PRFCHLLAVASWVSGFTNSSSFTFWVPLCGHRQVDHFFCEVPALLRLSCVDTHVNELTLMITSSIFVIPLILILTAIRAVLRMQSTTGLFGTCGAHLMAVSLFFIVT",
+        "INDNPPIFKGSEQRIFIPENSRFPLEDADIGANSLLTYTLELSKSLSLELRKSLDREETPELQLLLTATDGGKPELEGAVRLQITVLDVNDNAPVFDQAVYRAQLT"
+        "ESLVTTLNATDADEGVNGEVVFSFGNDVSPDFKVDSISGEIRVIGDLKVLDVNDNVPELMITSLSLPIKEDAPLNTVVALIKVSIDSGVNGQVTCSLSPHLPFKLVS",
+        "INDNPPVFKGAEQRIFIPENSRFPLEDADIGANSLLTYTLELSKSLSLELRKSLDREETPELQLLLTATDGGKPELEGTVRLQITVLDVNDNAPLFDQAIYRAQLV"
+        "ESLVTTLNATDADEGVNGEVVFSFGNDVSLDFNVDSLSGEIRVIGDLKVLDINDNAPELSITSLSLPIKEDTPLNTIIALIKVSIDSGVNGQVTCSLTPHVPFKLVS",
+        "INDNPPVFRGREQIIFIPESSRFPIEDADIGANALLTYTLELSKSLWLELRKSLDREETPELHLLLTATDGGKPELQGTVELLITVLDVNDNAPLFDQAVYRVLLL"
+        "ETLMTTLNASDADEGVNGEVVFSFDSGISRDFKVDSSSGEIRLIDKLKVLDVNDNAPELAVTSLYLPIREDAPLSTVIALITVSRDSGANGQVTCSLMPHVPFKLVS"]
+
+weight = "BLOSUM62.txt"
+
+
+# Calculates distance matrix of sequences
+def getDistanceMatrix(seqs, weight):
+    weightDict = {}
+    weightHead = []
+    file = open(weight, "r")
+    line = file.readline()
+    weightHead = line.split()[1:]
+    while line:
+        line = file.readline()
+        lineArr = line.split()
+        if len(lineArr) > 0:
+            weightDict[lineArr[0]] = lineArr[1:]
+    distMat = [[float('inf') for i in range(len(seqs))] for j in range(len(seqs))]
+    for i in range(len(distMat)-1):
+        for j in range(i+1, len(distMat[0])):
+            distMat[i][j] = calculateD(seqs[i], seqs[j], weightHead, weightDict)
+            distMat[j][i] = distMat[i][j]
+    return distMat
+
+
+# calculates d(i,j)
+def calculateD(seqi, seqj, header, dict):
+    p, qi, qj = 0, 0, 0
+    for k in range(len(seqi)):
+        ai = seqi[k]
+        aj = seqj[k]
+        indexi = header.index(ai)
+        indexj = header.index(aj)
+        p += float(dict[ai][indexj])
+        qi += float(dict[ai][indexi])
+        qj += float(dict[aj][indexj])
+    return 1 - (p / max(qi, qj))
+
+
+# Neighbor-Joining algorithm
+def neighborJoining(matrix, seqs, newNJNode):
+    print(matrix)
+    if len(seqs) == 2:
+        print("Creer noeud ayant pour enfant gauche : " + str(min(seqs[0], seqs[1])))
+        print(min(seqs[0], seqs[1]))
+        print("et pour enfant droit : " + str(max(seqs[0], seqs[1])))
+        print("Fin de Neighbor-Joining")
+        return
+    totalDistances = getTotalDistances(matrix)
+    njMatrix = getNJMatrix(matrix, totalDistances)
+    #mini, minj = findMinimum(njMatrix)
+    mini, minj = findMinimum(matrix)
+    limbLengthi = 0.5 * (matrix[mini][minj] + (totalDistances[mini] - totalDistances[minj]) / (len(seqs)-2))
+    limbLengthj = matrix[mini][minj] - limbLengthi
+    print("Creer noeud ayant pour enfant gauche : " + str(min(mini, minj)))
+    print(seqs[min(mini, minj)])
+    print("et pour enfant droit : " + str(max(mini, minj)))
+    print(seqs[max(mini, minj)])
+    newMatrix, newSeqs, newNJNode = createNewNJ(matrix, seqs, mini, minj, newNJNode)
+    return neighborJoining(newMatrix, newSeqs, newNJNode)
+
+
+# Returns the sum of each line of an array
+def getTotalDistances(matrix):
+    total = []
+    for s in matrix:
+        total.append(sum(s))
+    return total
+
+
+# Creates the matrix used for Neighbor-Joining algorithm
+def getNJMatrix(matrix, totDist):
+    njMat = [[0 for i in range(len(matrix))] for j in range(len(matrix[0]))]
+    for i in range(len(njMat)-1):
+        for j in range(i+1, len(njMat[0])):
+            njMat[i][j] = (len(matrix)-2) * matrix[i][j] - totDist[i] - totDist[j]
+            njMat[j][i] = njMat[i][j]
+    return njMat
+
+
+# Returns the minimal value of matrix and its indexes
+def findMinimum(matrix):
+    mins = []
+    for l in matrix:
+        mins.append(min(l))
+    minimum = min(mins)
+    mini = mins.index(minimum)
+    minj = matrix[mini].index(minimum)
+    return mini, minj
+
+
+def createNewNJ(matrix, seqs, indexi, indexj, newNJNode):
+    newMatrix = []
+    lastLine = []
+    for i in range(len(matrix)):
+        if i != indexi and i != indexj:
+            line = []
+            for j in range(len(matrix[0])):
+                if j != indexi and j != indexj:
+                    line.append(matrix[i][j])
+            newCell = (matrix[indexi][i] + matrix[indexj][i] - matrix[indexi][indexj]) * 0.5
+            line.append(newCell)
+            lastLine.append(newCell)
+            newMatrix.append(line)
+    lastLine.append(float('inf'))
+    newMatrix.append(lastLine)
+    seqs.pop(max(indexi, indexj))
+    seqs.pop(min(indexi, indexj))
+    seqs.append("NewNJNode_" + str(newNJNode))
+    newNJNode += 1
+    return newMatrix, seqs, newNJNode
+
+
+# q2.1
+distMat = getDistanceMatrix(seqs, weight)
+#print(distMat)
+# q2.2
+neighborJoining(distMat, seqs, 1)
+# Tests
+
+""" FIN DU CODE QUE NICO A AJOUTÉ """
 
 """ Gestion des proteines: Creation de dictionaire sans gaps """
 proteines = readFastaFile("proteines.fasta")
